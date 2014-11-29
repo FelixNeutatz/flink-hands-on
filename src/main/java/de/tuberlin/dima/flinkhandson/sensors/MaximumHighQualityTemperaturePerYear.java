d6bb3954563bed0c56a9a9458efb6e296fa00f18
FelixNeutatz <neutatz@googlemail.com>
2014-11-29 12:23:56 +0100

    my solution

--- src/main/java/de/tuberlin/dima/flinkhandson/sensors/MaximumHighQualityTemperaturePerYear.java
+++ src/main/java/de/tuberlin/dima/flinkhandson/sensors/MaximumHighQualityTemperaturePerYear.java
@@ -19,12 +19,14 @@
 package de.tuberlin.dima.flinkhandson.sensors;
 
 import de.tuberlin.dima.flinkhandson.Config;
+import org.apache.commons.math.stat.descriptive.rank.Max;
 import org.apache.flink.api.java.DataSet;
 import org.apache.flink.api.java.ExecutionEnvironment;
 import org.apache.flink.api.java.aggregation.Aggregations;
 import org.apache.flink.api.java.tuple.Tuple2;
 import org.apache.flink.api.java.tuple.Tuple4;
 import org.apache.flink.core.fs.FileSystem;
+import org.apache.flink.api.common.functions.FilterFunction;
 
 public class MaximumHighQualityTemperaturePerYear {
 
@@ -37,7 +39,7 @@ public class MaximumHighQualityTemperaturePerYear {
 
     ExecutionEnvironment env = ExecutionEnvironment.getExecutionEnvironment();
 
-    double qualityThreshold = 0.99;
+    final double qualityThreshold = 0.99;
 
     DataSet<Tuple4<Short, Short, Integer, Double>> measurements =
         env.readCsvFile(Config.pathTo("temperatures.tsv"))
@@ -45,8 +47,23 @@ public class MaximumHighQualityTemperaturePerYear {
             .types(Short.class, Short.class, Integer.class, Double.class);
 
 
-    // IMPLEMENT THIS STEP
-    DataSet<Tuple4<Short, Short, Integer, Double>> highQualityMeasurements = null;
+    DataSet<Tuple4<Short, Short, Integer, Double>> highQualityMeasurements =
+              // filter orders
+              measurements.filter(
+                      new FilterFunction<Tuple4<Short, Short, Integer, Double>>() {
+                          @Override
+                          public boolean filter(Tuple4<Short, Short, Integer, Double> t) {
+                              // status filter
+                              if(t.f3 >= qualityThreshold) {
+                                  return true;
+                              }
+                              return false;
+                          }
+                      });
+
+
+
+      highQualityMeasurements.print();
 
     DataSet<Tuple2<Short, Integer>> maxTemperatures =
         highQualityMeasurements
@@ -55,6 +72,7 @@ public class MaximumHighQualityTemperaturePerYear {
             .project(YEAR_FIELD, TEMPERATURE_FIELD).types(Short.class, Integer.class);
 
     maxTemperatures.writeAsCsv(Config.outputPathTo("maximumHighQualityTemperatures"), FileSystem.WriteMode.OVERWRITE);
+
 
     env.execute();
   }
